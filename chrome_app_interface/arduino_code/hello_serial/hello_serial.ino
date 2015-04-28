38119764bbb818d6fa819799c97d3a434af6b916
stephenfortune <stephen.j.fortune@gmail.com>
2015-04-28 12:49:37 +0100

    incorporating the code for communicating with Chrome via Serial as a subdirectory

---
+++ chrome_app_interface/arduino_code/hello_serial/hello_serial.ino
@@ -0,0 +1,49 @@
+#define LED 13
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(LED, OUTPUT);
+ 
+  // Turn Led off
+  digitalWrite(LED, LOW);
+
+  // Print the led Status to the serial in JSON format
+  printLEDStatus();
+}
+
+int incomingByte = 0;
+void loop() {
+
+  
+  // Check if there's a serial message waiting.
+  if (Serial.available() > 0) {
+    // If there is, read the incoming byte.
+    incomingByte = Serial.read();
+    
+    // if "y" turn on the led, otherwise turn off 
+    if (incomingByte == 'y') {
+      digitalWrite(LED, HIGH);
+    } else if (incomingByte == 'n') {
+      digitalWrite(LED, LOW);
+    }
+
+    printLEDStatus();
+  }
+  
+  delay(1000);
+
+}
+
+// Send the Led status via serial in JSON format
+void printLEDStatus () {
+   // read current LED status
+    int ledStatus = digitalRead(LED);
+    
+    // Create the JSON to send
+    String json = "{\"ledStatus\": " + String(ledStatus) + "}";
+    
+    // Print JSON via serial
+    Serial.println(json);
+  
+}
