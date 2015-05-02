17e3a6180dfde1d3ed034449d867bcef4c778918
stephenfortune <stephen.j.fortune@gmail.com>
2015-05-03 00:53:38 +0100

    time seeking function currently caught in a loop where it isn't grabbing all of the arrayBuffer

--- chrome_app_interface/arduino_code/mve/mve.ino
+++ chrome_app_interface/arduino_code/mve/mve.ino
@@ -1,7 +1,9 @@
 // the minimum viable evolvinator
 
   #include <Time.h>
-  
+  #define TIME_MSG_LEN  11   // time sync to PC is HEADER followed by unix time_t as ten ascii digits
+  #define TIME_HEADER  'T'   // Header tag for serial time sync message
+
   unsigned long currentMs;
   unsigned long msElapsedPrestart;
   unsigned long tUnixStart;                 // unix time at run start
@@ -78,15 +80,25 @@
     delay(1000);
     
     if (Serial.available()) {
-    
+        while(Serial.available() >=  TIME_MSG_LEN ){  // time message consists of a header and ten ascii digits
+          char c = Serial.read() ; 
+          //Serial.print(c);  
+          if( c == TIME_HEADER ) {       
+            time_t pctime = 0;
+            for(int i=0; i < TIME_MSG_LEN -1; i++){   
+              c = Serial.read();          
+              if( c >= '0' && c <= '9'){   
+                pctime = (10 * pctime) + (c - '0') ; // convert digits to a number    
+              }
+            }   
+            setTime(pctime);   // Sync Arduino clock to the time received on the serial port
+          }
+        }
     }
   }
   
   void dataRead(){
     // a function to handle storing data
-    if (Serial.available()) {
-    
-    }
   } 
    
   void startRun() {
@@ -98,7 +110,7 @@
     tUnix = tUnixStart + tElapsed;
     // SDInitialize();
     digitalWrite(pinValve, HIGH);          // open air valve ~ subsitute with a LED with attendant change on the logic
-    delay(2000);
+    delay(10000);
   }
   
   void SensorRead(){
